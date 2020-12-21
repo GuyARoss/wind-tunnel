134660fb6b9f73c916ebdd0d2cf324938a4c06c1
Guy Ross <guyallenross@gmail.com>
2020-12-21 11:10:42 -0500

    wip; build cmd

---
+++ cmd/build/main.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+	// validate schema
+	// generate client wrappers
+	//  -- schema defintion to struct
+	//  -- setup calling of the client
+	//  -- Dockerfilex
+}
---
+++ cmd/runtime/main.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	// runtime
+	// startup: start the client
+
+	// loop:
+	// incoming grpc request
+	// validate input
+	// call client
+	// validate output
+	// pass to next stage
+}
